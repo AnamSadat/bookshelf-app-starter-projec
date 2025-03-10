691b0203005f070b0844a0e83350e9a8cc9aedfc
AnamSadat <anamsadat3@gmail.com>
2025-03-10 15:05:58 +0700

    update fitur

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,20 @@ const SAVED_EVENT = "save-book";
 
 document.addEventListener("DOMContentLoaded", function () {
   const submitBook = document.getElementById("bookFormSubmit");
+  const searchSubmit = document.getElementById("searchSubmit");
+
   document.querySelector("#incompleteBookList").innerHTML = "";
   document.querySelector("#completeBookList").innerHTML = "";
 
   submitBook.addEventListener("click", function (event) {
     event.preventDefault();
     addBook();
+  });
+
+  searchSubmit.addEventListener("click", function (event) {
+    event.preventDefault();
+    searchBook();
+    document.getElementById("searchBookTitle").value = "";
   });
 
   if (isStorageExist()) {
@@ -27,8 +35,8 @@ function generateBookObject(id, title, author, year, isCompleted) {
     id,
     title,
     author,
-    year,
-    isCompleted,
+    year: parseInt(year),
+    isCompleted: Boolean(isCompleted),
   };
 }
 
@@ -47,14 +55,18 @@ function addBook() {
   document.querySelector("#bookFormAuthor").value = "";
   document.querySelector("#bookFormIsComplete").checked = false;
   document.dispatchEvent(new Event(RENDER_EVENT));
+
+  if (isCompleted) {
+    console.log("Selesai dibaca");
+  } else {
+    console.log("Belum selesai dibaca");
+  }
   saveData();
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-  console.log("🔄 Rendering Books...");
-  console.log("📚 Isi bookArray:", bookArray); // Tambahkan ini
+  console.log("Isi bookArray:", bookArray);
   renderBooks();
-  console.log("");
   saveData();
 });
 
@@ -80,21 +92,18 @@ function createBookElement(bookObject) {
     .querySelector("[data-testid='bookItemIsCompleteButton']")
     .addEventListener("click", function () {
       switchBook(bookObject.id);
-      console.log("Berhasil dipindahkan");
     });
 
   bookItem
     .querySelector("[data-testid='bookItemDeleteButton']")
     .addEventListener("click", function () {
       removeBook(bookObject.id);
-      console.log("Berhasil dihapus");
     });
 
   bookItem
     .querySelector("[data-testid='bookItemEditButton']")
     .addEventListener("click", function () {
       editBook(bookObject.id);
-      console.log("Berhasil Edit");
     });
 
   return bookItem;
@@ -106,14 +115,14 @@ function renderBooks() {
 
   incompleteBookList.innerHTML = "";
   completeBookList.innerHTML = "";
+
   bookArray.forEach((book) => {
     const bookElement = createBookElement(book);
+
     if (book.isCompleted) {
       completeBookList.append(bookElement);
-      console.log("sudah selesai");
     } else {
       incompleteBookList.append(bookElement);
-      console.log("belum selesai");
     }
   });
 }
@@ -130,15 +139,14 @@ function findBookIndex(bookId) {
 
 function switchBook(bookId) {
   let bookIndex = findBookIndex(bookId);
-  console.log("bookId = ", bookId);
 
   if (bookIndex !== -1) {
     bookArray[bookIndex].isCompleted = !bookArray[bookIndex].isCompleted;
-    console.log("status = ", bookArray[bookIndex].isCompleted);
+    console.log("status isCompleted = ", bookArray[bookIndex].isCompleted);
   } else {
     console.log("Buku tidak ditemukan");
   }
-
+  console.log("Berhasil dipindahkan");
   document.dispatchEvent(new Event(RENDER_EVENT));
   saveData();
 }
@@ -146,25 +154,11 @@ function switchBook(bookId) {
 function removeBook(bookId) {
   // Buat elemen modal
   const modal = document.createElement("div");
-  modal.classList.add("container-pop-up");
-  modal.style.position = "fixed";
-  modal.style.zIndex = "100";
-  modal.style.left = "0";
-  modal.style.top = "0";
-  modal.style.width = "100%";
-  modal.style.height = "100%";
-  modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  modal.style.display = "flex";
-  modal.style.justifyContent = "center";
-  modal.style.alignItems = "center";
+  modal.classList.add("modal-pop-up");
 
   // Buat konten modal
   const modalContent = document.createElement("div");
-  modalContent.classList.add("remove-content");
-  modalContent.style.background = "white";
-  modalContent.style.padding = "20px";
-  modalContent.style.borderRadius = "8px";
-  modalContent.style.textAlign = "center";
+  modalContent.classList.add("modal-remove");
 
   // Tambahkan teks konfirmasi
   const modalText = document.createElement("p");
@@ -175,6 +169,7 @@ function removeBook(bookId) {
   confirmButton.classList.add("btn-confirm-delete");
   confirmButton.innerText = "Hapus";
   confirmButton.style.margin = "5px";
+
   confirmButton.addEventListener("click", function () {
     const bookIndex = findBookIndex(bookId);
     if (bookIndex !== -1) {
@@ -188,6 +183,7 @@ function removeBook(bookId) {
   const cancelButton = document.createElement("button");
   cancelButton.innerText = "Tidak";
   cancelButton.style.margin = "5px";
+
   cancelButton.addEventListener("click", function () {
     document.body.removeChild(modal); // Hapus modal tanpa menghapus buku
   });
@@ -199,6 +195,7 @@ function removeBook(bookId) {
   modal.appendChild(modalContent);
 
   // Tambahkan modal ke dalam body
+  console.log("Berhasil dihapus");
   document.body.appendChild(modal);
   saveData();
 }
@@ -214,46 +211,47 @@ function editBook(bookId) {
 
   // Buat formulir edit
   const editForm = document.createElement("div");
-  editForm.classList.add("container-pop-up");
+  editForm.classList.add("modal-pop-up");
 
   editForm.innerHTML = `
-      <div class="edit-container">
-         <h2>Edit Buku</h2>
-          <form id="bookForm" data-testid="bookForm">
-            <div class="judul">
-              <label class="label" for="bookFormTitle">Judul</label>
-              <input
-                id="editTitle"
-                type="text"
-                required
-                value="${book.title}"
-                data-testid="bookFormTitleInput"
-              />
-            </div>
-            <div>
-              <label class="label" for="bookFormAuthor">Penulis</label>
-              <input
-                id="editAuthor"
-                type="text"
-                required
-                value="${book.author}"
-                data-testid="bookFormAuthorInput"
-              />
-            </div>
-            <div>
-              <label class="label" for="bookFormYear">Tahun</label>
-              <input
-                id="editYear"
-                type="number"
-                required
-                value="${book.year}"
-                data-testid="bookFormYearInput"
-              />
-            </div>
-            
-            <button id="saveEdit">Simpan</button>
-      <button id="cancelEdit">Batal</button>
-          </form>
+      <div class="modal-edit">
+        <h2>Edit Buku</h2>
+        <form id="bookForm" class="align" data-testid="bookForm">
+          <div class="judul">
+            <label class="label" for="bookFormTitle">Judul</label>
+            <input
+              id="editTitle"
+              type="text"
+              required
+              value="${book.title}"
+              data-testid="bookFormTitleInput"
+            />
+          </div>
+          <div>
+            <label class="label" for="bookFormAuthor">Penulis</label>
+            <input
+              id="editAuthor"
+              type="text"
+              required
+              value="${book.author}"
+              data-testid="bookFormAuthorInput"
+            />
+          </div>
+          <div>
+            <label class="label" for="bookFormYear">Tahun</label>
+            <input
+              id="editYear"
+              type="number"
+              required
+              value="${book.year}"
+              data-testid="bookFormYearInput"
+            />
+          </div>
+          <div class="btn-edit">
+            <button id="btn-save-edit">Simpan</button>
+            <button id="btn-cancel-edit">Batal</button>
+          </div>
+        </form>
       </div>
 
       
@@ -262,96 +260,47 @@ function editBook(bookId) {
 
   // Event Listener untuk Simpan Edit
   document
-    .getElementById("saveEdit")
+    .getElementById("btn-save-edit")
     .addEventListener("click", function (event) {
       event.preventDefault();
       bookArray[bookIndex].title = document.getElementById("editTitle").value;
       bookArray[bookIndex].author = document.getElementById("editAuthor").value;
       bookArray[bookIndex].year = document.getElementById("editYear").value;
-      console.log("sudah edit");
-
-      console.log("Index buku yang diedit:", bookIndex);
-      console.log("Data sebelum diedit:", bookArray[bookIndex]);
+      console.log("Berhasil Edit");
 
       document.dispatchEvent(new Event(RENDER_EVENT));
       document.body.removeChild(editForm);
     });
 
   // Event Listener untuk Batal
-  document.getElementById("cancelEdit").addEventListener("click", function () {
-    document.body.removeChild(editForm);
-  });
+  document
+    .getElementById("btn-cancel-edit")
+    .addEventListener("click", function () {
+      document.body.removeChild(editForm);
+    });
   saveData();
 }
 
 function searchBook() {
   const query = document.getElementById("searchBookTitle").value.toLowerCase();
+  const books = bookArray;
 
-  // Mencari buku berdasarkan judul
-  const foundBook = bookArray.find(
-    (book) => book.title.toLowerCase() === query
-  );
+  incompleteBookList.innerHTML = "";
+  completeBookList.innerHTML = "";
 
-  // Jika buku tidak ditemukan, tampilkan pesan
-  if (!foundBook) {
-    alert("Buku tidak ditemukan!");
-    return;
-  }
-
-  // Membuat modal pop-up
-  const searchPopUp = document.createElement("div");
-  searchPopUp.classList.add("container-pop-up");
-
-  const searchPopUpContent = document.createElement("div");
-  searchPopUpContent.classList.add("search-pop-up-content");
-
-  const searchPopUpTitle = document.createElement("h2");
-
-  if (foundBook.isCompleted) {
-    searchPopUpTitle.innerText = "Sudah Selesai";
-  } else {
-    searchPopUpTitle.innerText = "Belum Selesai";
-  }
-
-  const searchPopUpContentText = document.createElement("div");
-  searchPopUpContentText.id = "search-pop-up-content-text";
-
-  const searchPopUpText = document.createElement("p");
-  searchPopUpText.innerHTML = `
-
-    <h3 data-testid="bookItemTitle">${foundBook.title}</h3>
-    <p data-testid="bookItemAuthor">Penulis: ${foundBook.author}</p>
-    <p data-testid="bookItemYear">Tahun: ${foundBook.year}</p>
-    
-  `;
-
-  // Tombol Oke
-  const okButton = document.createElement("button");
-  okButton.classList.add("okeButton");
-  okButton.innerText = "Oke";
-  okButton.style.margin = "5px";
-  okButton.addEventListener("click", function () {
-    document.body.removeChild(searchPopUp);
+  books.forEach((book) => {
+    if (book.title.toLowerCase().includes(query)) {
+      const bookElement = createBookElement(book);
+      if (book.isCompleted) {
+        completeBookList.appendChild(bookElement);
+      } else {
+        incompleteBookList.appendChild(bookElement);
+      }
+    } else {
+      console.log("Maaf, buku tidak ditemukan");
+    }
   });
-
-  // Menyusun modal
-  searchPopUpContentText.appendChild(searchPopUpText);
-  searchPopUpContent.appendChild(searchPopUpTitle);
-  searchPopUpContent.appendChild(searchPopUpContentText);
-  searchPopUpContent.appendChild(okButton);
-  searchPopUp.appendChild(searchPopUpContent);
-
-  // Tambahkan modal ke dalam body
-  document.body.appendChild(searchPopUp);
 }
-
-document
-  .getElementById("searchSubmit")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    searchBook();
-    document.getElementById("searchBookTitle").value = "";
-  });
 
 function saveData() {
   if (isStorageExist()) {
@@ -360,6 +309,10 @@ function saveData() {
     document.dispatchEvent(new Event(SAVED_EVENT));
   }
 }
+
+document.addEventListener(SAVED_EVENT, () => {
+  console.log("Data berhasil di simpan.");
+});
 
 function isStorageExist() {
   if (typeof Storage === undefined) {
